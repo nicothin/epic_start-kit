6c479082d2c69fc5286156a39a83a23521ab719d
Nikolay Gromov <nicothin@gmail.com>
2016-08-18 21:17:30 +0300

    demo refactoring

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
 'use strict';
 
+// Читаем содержимое package.json в константу
 const pjson = require('./package.json');
+// Получим из константы другую константу с адресами папок сборки и исходников
 const dirs = pjson.config.directories;
 
+// Определим необходимые инструменты
 const gulp = require('gulp');
 const less = require('gulp-less');
 const rename = require('gulp-rename');
@@ -19,90 +22,106 @@ const newer = require('gulp-newer');
 const imagemin = require('gulp-imagemin');
 const pngquant = require('imagemin-pngquant');
 
-// Компиляция препроцессора
+// ЗАДАЧА: Компиляция препроцессора
 gulp.task('less', function(){
-  return gulp.src(dirs.source + '/less/style.less')
-    .pipe(sourcemaps.init())
-    .pipe(less())
-    .pipe(rename('style.css'))
-    .pipe(postcss([
-        autoprefixer({browsers: ['last 2 version']}),
-        mqpacker({
-          sort: true
-        }),
+  return gulp.src(dirs.source + '/less/style.less')         // какой файл компилировать (путь из константы)
+    .pipe(sourcemaps.init())                                // инициируем карту кода
+    .pipe(less())                                           // компилируем LESS
+    .pipe(rename('style.css'))                              // переименовываем
+    .pipe(postcss([                                         // делаем постпроцессинг
+        autoprefixer({ browsers: ['last 2 version'] }),     // автопрефиксирование
+        mqpacker({ sort: true }),                           // объединение медиавыражений
     ]))
-    .pipe(sourcemaps.write('/'))
-    .pipe(gulp.dest(dirs.build + '/css/'))
-    .pipe(browserSync.stream());
+    .pipe(sourcemaps.write('/'))                            // записываем карту кода как отдельный файл (путь из константы)
+    .pipe(gulp.dest(dirs.build + '/css/'))                  // записываем CSS-файл (путь из константы)
+    .pipe(browserSync.stream());                            // обновляем в браузере
 });
 
-// Сборка HTML
+// ЗАДАЧА: Сборка HTML
 gulp.task('html', function() {
-  return gulp.src(dirs.source + '/*.html')
-    .pipe(fileinclude({
+  return gulp.src(dirs.source + '/*.html')                  // какие файлы обрабатывать (путь из константы, маска имени)
+    .pipe(fileinclude({                                     // обрабатываем gulp-file-include
       prefix: '@@',
       basepath: '@file',
       indent: true,
     }))
-    .pipe(replace(/\n\s*<!--DEV[\s\S]+?-->/gm, ''))
-    .pipe(gulp.dest(dirs.build));
+    .pipe(replace(/\n\s*<!--DEV[\s\S]+?-->/gm, ''))         // убираем комментарии <!--DEV ... -->
+    .pipe(gulp.dest(dirs.build));                           // записываем файлы (путь из константы)
 });
 
-// Копирование и оптимизация изображений
+// ЗАДАЧА: Копирование и оптимизация изображений
 gulp.task('img', function () {
-  return gulp.src(dirs.source + '/img/*.{gif,png,jpg,jpeg,svg}', {since: gulp.lastRun('img')}) // только для изменившихся с последнего запуска файлов
-    .pipe(newer(dirs.build + '/img'))  // оставить в потоке только изменившиеся файлы
-    .pipe(imagemin({
+  return gulp.src(
+    dirs.source + '/img/*.{gif,png,jpg,jpeg,svg}',          // какие файлы обрабатывать (путь из константы, маска имени, много расширений)
+    {since: gulp.lastRun('img')                             // оставим в потоке обработки только изменившиеся от последнего запуска задачи (в этой сессии) файлы
+  })
+    .pipe(newer(dirs.build + '/img'))                       // оставить в потоке только новые файлы (сравниваем с содержимым папки билда)
+    .pipe(imagemin({                                        // обрабатываем
         progressive: true,
         svgoPlugins: [{removeViewBox: false}],
         use: [pngquant()]
     }))
-    .pipe(gulp.dest(dirs.build + '/img'));
+    .pipe(gulp.dest(dirs.build + '/img'));                  // записываем файлы (путь из константы)
 });
 
-// Очистка папки сборки
+// ЗАДАЧА: Очистка папки сборки
 gulp.task('clean', function () {
-  return del([
-    dirs.build + '/**/*',
-    '!' + dirs.build + '/readme.md'
+  return del([                                              // стираем
+    dirs.build + '/**/*',                                   // все файлы из папки сборки (путь из константы)
+    '!' + dirs.build + '/readme.md'                         // кроме readme.md (путь из константы)
   ]);
 });
 
-// Сборка всего
-gulp.task('build', gulp.series(
-  'clean',
-  gulp.parallel('less'),
-  'html'
+// ЗАДАЧА: Сборка всего
+gulp.task('build', gulp.series(                             // последовательно:
+  'clean',                                                  // последовательно: очистку папки сборки
+  gulp.parallel('less'),                                    // параллельно: компиляцию стилей, ...
+  'html'                                                    // последовательно: сборку разметки
 ));
 
-// Локальный сервер, слежение
+// ЗАДАЧА: Локальный сервер, слежение
 gulp.task('serve', gulp.series('build', function() {
-  browserSync.init({
-    server: dirs.build,
-    port: 3000,
-    startPath: 'index.html'
+
+  browserSync.init({                                        // запускаем локальный сервер (показ, автообновление, синхронизацию)
+    server: dirs.build,                                     // папка, которая будет «корнем» сервера (путь из константы)
+    port: 3000,                                             // порт, на котором будет работать сервер
+    startPath: 'index.html',                                // файл, который буде открываться в браузере при старте сервера
+    // open: false                                          // возможно, каждый раз стартовать сервер не нужно...
   });
-  gulp.watch([
-    dirs.source + '/*.html',
-    dirs.source + '/_include/*.html',
-  ], gulp.series('html', reloader));
-  gulp.watch(dirs.source + '/less/**/*.less', gulp.series('less'));
-  gulp.watch(dirs.source + '/img/*.{gif,png,jpg,jpeg,svg}', gulp.series('img', reloader));
+
+  gulp.watch(                                               // следим за HTML
+    [
+      dirs.source + '/*.html',                              // в папке с исходниками
+      dirs.source + '/_include/*.html',                     // и в папке с мелкими вставляющимся файлами
+    ],
+    gulp.series('html', reloader)                           // при изменении файлов запускаем пересборку HTML и обновление в браузере
+  );
+
+  gulp.watch(                                               // следим за LESS
+    dirs.source + '/less/**/*.less',
+    gulp.series('less')                                     // при изменении запускаем компиляцию (обновление браузера — в задаче компиляции)
+  );
+
+  gulp.watch(                                               // следим за изображениями
+    dirs.source + '/img/*.{gif,png,jpg,jpeg,svg}',
+    gulp.series('img', reloader)                            // при изменении оптимизируем, копируем и обновляем в браузере
+  );
+
   // gulp.watch(blocks.js, gulp.series('js', reloader));
 }));
 
-// Отправка в GH pages (ветку gh-pages репозитория)
+// ЗАДАЧА, ВЫПОЛНЯЕМАЯ ТОЛЬКО ВРУЧНУЮ: Отправка в GH pages (ветку gh-pages репозитория)
 gulp.task('deploy', function() {
   return gulp.src('./build/**/*')
     .pipe(ghPages());
 });
 
-// Задача по умолчанию
+// ЗАДАЧА: Задача по умолчанию
 gulp.task('default',
   gulp.series('serve')
 );
 
-// Перезагрузка в браузере
+// Дополнительная функция для перезагрузки в браузере
 function reloader(done) {
   browserSync.reload();
   done();
--- readme.md
+++ readme.md
@@ -1,7 +1,21 @@
-# EPIC starter kit
+# EPIC starter kit (машины должны страдать)
 
-Установка: `npm i`
+Установка: `npm i`.
 
-Запуск: `npm start`
+Запуск: `npm start`.
+
+Запуск конкретной задачи: `npm start имя_задачи` (список задач смотри в `gulpfile.js`)
 
 Остановка: <kbd>Ctrl + C</kbd>
+
+
+
+## К прочтению / ознакомлению
+
+- [Как работать с CSS-препроцессорами и БЭМ](http://nicothin.github.io/idiomatic-pre-CSS/)
+- [Шпаргалка по bash](https://github.com/nicothin/web-development/tree/master/bash)
+- [Шпаргалка по консольным командам Git](https://github.com/nicothin/web-development/tree/master/git)
+- [Шпаргалка ниндзя Sublime Text 3](http://nicothin.github.io/sublime-text/sublime-text-3-hotkeys.html)
+- [Sublime Text 3 для работы с фронтэндом](https://github.com/nicothin/sublime-text)
+- [Gulp для самых маленьких](https://www.youtube.com/watch?v=vW51JUVT66w) (видео)
+- [Скринкаст по Gulp](https://www.youtube.com/playlist?list=PLDyvV36pndZFLTE13V4qNWTZbeipNhCgQ) (серия видео)
