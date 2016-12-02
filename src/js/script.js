0798a715d61f4d13b09840cf3086f7917be875bd
Nikolay Gromov <nicothin@gmail.com>
2016-12-03 00:58:41 +0300

    Занятие по jQuery

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,27 @@
 $(document).ready(function(){
-  // весь ваш код
+
+  $('#gallery-thumbs').owlCarousel({
+    items: 3,
+    loop: true,
+    center: true,
+    responsive: {
+      680: {
+        items: 4,
+      },
+      800: {
+        items: 5,
+      },
+    }
+  });
+
+  var mainImageUrl = $('#gallery-start-photo').attr('href');
+
+  $('#gallery-main-pict').html('<img src="'+mainImageUrl+'" alt="">');
+
+  $('#gallery-thumbs a').on('click', function(e){
+    e.preventDefault();
+    var imageUrl = $(this).attr('href');
+    $('#gallery-main-pict').html('<img src="'+imageUrl+'" alt="">');
+  });
+
 });
